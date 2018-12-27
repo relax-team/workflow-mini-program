301cc2fab799f32bd0df9064548a8c043df3e89d
xiaqingsong <xqslove@yeah.net>
2018-12-27 18:32:21 +0800

    增加侧滑删除组件

--- src/subPages/demo/index.js
+++ src/subPages/demo/index.js
@@ -1,10 +1,20 @@
 import regeneratorRuntime from '../../libs/regenerator-runtime';
 import cfg from '../../config/index';
+
 const app = getApp();
 Page({
     data: {
         userInfo: {},
-        quantity: 1
+        quantity: 1,
+        slideMenu: {
+            list: [{
+                id: 123,
+                title: '测试侧滑菜单组件123'
+            }, {
+                id: 456,
+                title: '测试侧滑菜单组件456'
+            }]
+        }
     },
     onLoad() {
 
@@ -12,9 +22,15 @@ Page({
     /*
     * 数量变化
     * */
-    onQuantityChange(e){
+    onQuantityChange(e) {
         this.setData({
             quantity: e.detail
         });
     },
+    /*
+    * 侧滑菜单操作
+    * */
+    onDelete(e) {
+        console.log('当前删除的ID为： ', e.detail)
+    }
 });
