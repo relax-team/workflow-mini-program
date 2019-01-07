8e340245e921a5abe8280a7a9b3602efdd40a5d3
xiaqingsong <xqslove@yeah.net>
2019-01-07 15:24:44 +0800

    新增详情页

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,7 +1,7 @@
 import regeneratorRuntime from '../../libs/regenerator-runtime';
 import cfg from '../../config/index';
 
-const app = getApp()
+const app = getApp();
 Page({
     data: {
         scrollLeft: 0,
@@ -19,6 +19,12 @@ Page({
             {name: '吧腐', id: 'h'},
             {name: '密码', id: 'h'}
         ],
+        banner: [
+            '//m.360buyimg.com/mobilecms/s750x366_jfs/t1/23360/8/4031/468576/5c2dc861E189161bc/bac4293f6060aa5f.jpg!cr_1125x549_0_72!q70.jpg.dpg',
+            '//m.360buyimg.com/mobilecms/s750x366_jfs/t1/28271/11/4014/197362/5c2dece0E62c983d6/63877ba3cf1d8e36.jpg!cr_1125x549_0_72!q70.jpg.dpg',
+            '//m.360buyimg.com/mobilecms/s750x366_jfs/t23554/196/252614617/149239/73b66270/5b2a6ecdNc10d686b.jpg!cr_1125x549_0_72!q70.jpg.dpg',
+            '//m.360buyimg.com/mobilecms/s750x366_jfs/t1/28339/2/4203/79668/5c2f0645E57c94f60/78268550cb01f021.jpg!cr_1125x549_0_72!q70.jpg.dpg',
+        ]
     },
 
     // 切换导航类目
