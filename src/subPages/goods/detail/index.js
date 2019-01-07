c24c00e0c6d68d7f5c292a73c41fe7f3c18339c5
xiaqingsong <xqslove@yeah.net>
2019-01-07 14:15:30 +0800

    优化组件样式

---
+++ src/subPages/goods/detail/index.js
@@ -0,0 +1,11 @@
+import regeneratorRuntime from '../../../libs/regenerator-runtime';
+
+const app = getApp();
+Page({
+    data: {
+        goodsBanner: ['//img12.360buyimg.com/n1/s450x450_jfs/t1/27491/2/4431/289258/5c321c30E280237c3/86ed242b2360a2cd.jpg']
+    },
+    onLoad() {
+
+    }
+});
