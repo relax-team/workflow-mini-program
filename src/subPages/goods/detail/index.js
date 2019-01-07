ad899c2e04d6cfcba9b88b971c294488a5255073
xiaqingsong <xqslove@yeah.net>
2019-01-07 14:45:14 +0800

    新增详情页

--- src/subPages/goods/detail/index.js
+++ src/subPages/goods/detail/index.js
@@ -3,7 +3,11 @@ import regeneratorRuntime from '../../../libs/regenerator-runtime';
 const app = getApp();
 Page({
     data: {
-        goodsBanner: ['//img12.360buyimg.com/n1/s450x450_jfs/t1/27491/2/4431/289258/5c321c30E280237c3/86ed242b2360a2cd.jpg']
+        goodsBanner: [
+            '//img12.360buyimg.com/n1/s450x450_jfs/t1/27491/2/4431/289258/5c321c30E280237c3/86ed242b2360a2cd.jpg',
+            '//img12.360buyimg.com/n1/s450x450_jfs/t1/18848/17/4571/212369/5c32ce88E1ddec98d/67af5d6fb1577cd3.jpg',
+            '//img12.360buyimg.com/n1/s450x450_jfs/t1/7211/16/8954/485449/5c1088ecE9884e066/542088e742e6169c.jpg'
+        ]
     },
     onLoad() {
 
