89567c3bf70fb3b10d271e57a65015a518f6faf0
xiaqingsong <xqslove@yeah.net>
2019-01-10 10:32:39 +0800

    新增gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,20 @@
 'use strict';
 
-// 引入 gulp
-var gulp = require('gulp');
-
-// 引入组件
-var replace = require('gulp-replace'),
+// 引入gulp组件
+var gulp = require('gulp'),
+    replace = require('gulp-replace'),
     clean = require('gulp-clean'),
     gulpif = require('gulp-if'),
     gutil = require('gulp-util'),
-    less = require('gulp-less'),
-    rename = require('gulp-rename'),
-    stripDebug = require('gulp-strip-debug');
+    runSequence = require('run-sequence'),
+    sftp = require('gulp-sftp');      // 自动部署静态资源
 
 var pkg = require('./package.json');
-
 var version = 'v' + pkg.version;
 var cfg = {
     entry: './src',
     output: './dist'
 };
-
 
 //构建开始提示
 gutil.log(gutil.colors.green('[Start build]'), '版本:', gutil.colors.magenta(version), ', 输出目录:', gutil.colors.magenta(cfg.output));
@@ -31,37 +26,40 @@ gulp.task('clean', function () {
         .pipe(clean());
 });
 
-// clone and replace
+// 拷贝文件到output目录
 gulp.task('clone', function () {
     return gulp.src([
-        'src/**/*',
-        '!src/icons/**',
+        'src/**',
+        '!src/project.config.json',
     ])
         .pipe(gulp.dest(cfg.output))
 });
 
-//处理图片
-gulp.task('image', function () {
-    return gulp.src([
-        'src/icons/tabbar/**'
-    ])
-        .pipe(gulp.dest(cfg.output + '/icons/tabbar'))
+// 上传FTP
+gulp.task('ftpStatic', function () {
+    return gulp.src('static/**')
+        .pipe(sftp({
+            host: `${host}`,
+            port: 20021,
+            user: `${user}`,
+            pass: `${pass}`,
+            remotePath: `${remotePath}`,
+        }));
 });
 
-//构建
-gulp.task('build', ['clean'], function () {
-    gulp.start(
-        'clone',
-        'image'
-    );
+
+//发布
+gulp.task('build', function () {
+    runSequence('clean', 'clone', function () {
+        gutil.log(gutil.colors.green('恭喜，构建完成！'));
+    });
 });
 
-//默认任务
-gulp.task('watch', ['build'], function () {
-    gulp.watch([
-            'src/**/*.*'
-        ],
-        ['build']
-    );
+//ftp
+gulp.task('ftp',function () {
+    runSequence('ftpStatic', function () {
+        gutil.log(gutil.colors.green('恭喜，FTP上传完成！'));
+    });
 });
 
+gulp.task('default', ['build']);
