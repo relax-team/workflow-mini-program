f3af482ac2285f1325cea6006bfd7c715e1278c3
xiaqingsong <xqslove@yeah.net>
2019-01-23 20:26:17 +0800

    新增规范

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+	root: true,
+	env: {
+		es6: true,
+		node: true
+	},
+	extends: 'eslint:recommended',
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: 'module'
+	},
+	rules: {
+		indent: ['error', 2],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		'no-console': 1
+	},
+	globals: {
+		getApp: false,
+		Page: false,
+		wx: false,
+		App: false,
+		getCurrentPages: false,
+		Component: false
+	}
+};
